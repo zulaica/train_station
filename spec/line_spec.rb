2714a73c1bd08c067f25fd4fc4d1e3579d7682a5
David Zulaica & Joey Sandine <student@epicodus.com>
2015-01-21 11:52:28 -0800

    Add Line#name and Line#id specs for reference.

--- spec/line_spec.rb
+++ spec/line_spec.rb
@@ -2,7 +2,7 @@ require('spec_helper')
 
 describe(Line) do
 
-  describe(".all") do
+  describe('.all') do
     it("is an empty list of train lines") do
       expect(Line.all()).to(eq([]))
     end
@@ -16,16 +16,27 @@ describe(Line) do
     end
   end
 
-  describe("#==") do
+  describe('#name') do
+    it("tells you the name of the line") do
+      test_line = Line.new({:name => "White Line", :id => nil})
+      expect(test_line.name()).to(eq("White Line"))
+    end
+  end
+
+  describe('#id') do
+    it("sets the ID when you save it") do
+      test_line = Line.new({:name => "White Line", :id => nil})
+      test_line.save()
+      expect(test_line.id()).to(be_an_instance_of(Fixnum))
+    end
+  end
+
+  describe('#==') do
     it("makes sure lines are equal if they share a name") do
       line1 = Line.new({:name => "White Line", :id => nil})
       line2 = Line.new({:name => "White Line", :id => nil})
       expect(line1).to(eq(line2))
     end
   end
-
-
-
-
 
 end
