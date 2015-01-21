b5f9e4a6197d8b4f0908a746ab6b5594d97d4483
David Zulaica & Joey Sandine <student@epicodus.com>
2015-01-21 15:54:23 -0800

    Add Stop#save method.

--- lib/stop.rb
+++ lib/stop.rb
@@ -3,8 +3,40 @@ class Stop
   attr_reader(:station_id, :line_id)
 
   define_method(:initialize) do |attributes|
-    @station_id = station_id
-    @line_id = line_id
+    ##########
+    #
+    # Dear Joey (Part 2. Don't worry about. It makes sense. I figured this part
+    # out after the problem below),
+    #
+    # So, basically, we stupid damn burned through declaring our stupid damn
+    # instance variables and didn't stupid damn fetch them from the stupid damn
+    # attributes. Also, since stupid damn databases return strings, I had to set
+    # the stupid damn id variables to stupid damn integers.
+    #
+    # Smooches,
+    # David
+    #
+    # # Broken Code Example
+    # @station_id = station_id
+    # @line_id = line_id
+    # #
+    #
+    ##########
+    @station_id = attributes.fetch(:station_id).to_i()
+    @line_id = attributes.fetch(:line_id).to_i()
+    ##########
+    #
+    # Dear Joey,
+    #
+    # So, basically, the stupid damn Stop#save was freaking out because we were
+    # assigning a stupid damn value to stupid damn @id without stupid damn
+    # declaring it first when we stupid damn initialized.
+    #
+    # Smooches,
+    # David
+    #
+    ##########
+    @id = nil
   end
 
   define_singleton_method(:all) do
@@ -18,6 +50,10 @@ class Stop
     stops
   end
 
+  define_method(:save) do
+    result = DB.exec("INSERT INTO stops (station_id, line_id) VALUES (#{@station_id}, #{@line_id}) RETURNING id;")
+    @id = result.first().fetch("id").to_i()
+  end
 
   define_method(:==) do |another_stop|
     self.station_id().eql?(another_stop.station_id()).&(self.line_id().eql?(another_stop.line_id()))
--- spec/stop_spec.rb
+++ spec/stop_spec.rb
@@ -8,6 +8,25 @@ describe(Stop) do
     end
   end
 
+  # describe(".find_station") do
+  #   it("return a list of lines for a station based on the station_id") do
+  #
+  #   end
+  # end
+
+  describe("#save") do
+    it("pairs a station's ID to a line's ID") do
+# binding.pry
+      test_station = Station.new({:name => "Gravel Pit", :id => nil})
+      test_station.save()
+      test_line = Line.new({:name => "White Line", :id => nil})
+      test_line.save()
+      test_stop = Stop.new({:id => nil, :station_id => test_station.id(), :line_id => test_line.id()})
+      test_stop.save()
+      expect(Stop.all()).to(eq([test_stop]))
+    end
+  end
+
   describe("#==") do
     it("makes sure stops are equal if they share a station_id") do
       stop1 = Stop.new({:station_id => 1, :line_id => 1})
